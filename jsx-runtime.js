21673a8d10520b2ee6165b0ddee7ade2e6b5d2af
Alan Quintiliano <alan_doni@hotmail.com>
2021-05-12 13:44:47 -0300

    Supporting text

--- jsx-runtime.js
+++ jsx-runtime.js
@@ -49,10 +49,15 @@ function render(element, container) {
       ? container.ownerDocument.createTextNode("")
       : container.ownerDocument.createElement(element.type);
 
-  if (!element.props && element.length > 0) {
+  if (!element.props && Array.isArray(element)) {
     element.forEach((el) => {
       render(el, container);
     });
+    return;
+  }
+  if (!element.props) {
+    dom = container.ownerDocument.createTextNode(element);
+    container.appendChild(dom);
     return;
   }
 
